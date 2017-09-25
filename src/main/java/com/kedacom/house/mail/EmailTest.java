a58ebe057b61184637b6f639f1382cc1e48a8203
chunyang.zhao <springdew84@126.com>
2017-09-25 18:03:49 +0800

    发送邮件

---
+++ src/main/java/com/kedacom/house/mail/EmailTest.java
@@ -0,0 +1,29 @@
+package com.kedacom.house.mail;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.kedacom.house.mail.Email;
+
+public class EmailTest {
+
+	@Test
+	public void sendEmail() throws Exception {
+		String userName = "11111111@qq.com"; // 发件人邮箱
+		String password = "********"; // 发件人密码
+		String smtpHost = "smtp.qq.com"; // 邮件服务器
+
+		String to = "22222222@qq.com"; // 收件人，多个收件人以半角逗号分隔
+		String cc = "33333333@qq.com"; // 抄送，多个抄送以半角逗号分隔
+		String subject = "这是邮件的主题"; // 主题
+		String body = "这是邮件的正文"; // 正文，可以用html格式的哟
+		List<String> attachments = Arrays.asList("D:\\tmp\\1.png", "D:\\tmp\\2.png"); // 附件的路径，多个附件也不怕
+
+		Email email = Email.entity(smtpHost, userName, password, to, cc, subject, body, attachments);
+
+		email.send(); // 发送！
+	}
+
+}
